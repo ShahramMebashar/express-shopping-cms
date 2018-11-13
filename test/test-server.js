4533b0835215f299476fdad0c9b7c60cd31b47bc
Shahram Mebashar <shahram.mebashar@gmail.com>
2018-11-13 19:22:18 +0300

    added test unit for project

---
+++ test/test-server.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should;
+chai.use(chaiHttp);
+
+describe('Home page', function() {
+    it('get /', function (done) {
+        done();
+    });
+});
